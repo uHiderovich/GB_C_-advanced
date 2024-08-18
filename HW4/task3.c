ec73d5ce1742485234ee96c7c37e85da9281d37d
Артём Гапеевцев <agapeevtsev@yandex.ru>
2024-08-18 17:45:49 +0800

    ДЗ 4 Задание 3

--- HW4/task3.c
+++ HW4/task3.c
@@ -13,51 +13,65 @@ monty_python
 python_has_list_comprehensions
 Данные на выход: 6
 */
-
 #include <stdio.h>
 #include <string.h>
 
-#define MAXLENGTH 10001
+#define MAXLENGTH 104
 
-void ComputePrefix(const char* str, int* pi, int n) {
-  int k = 0;
-  pi[0] = 0;
-
+// Функция для вычисления Z-функции
+void ZFunction(const char* s, int* z, int n) {
+  int l = 0, r = 0;
+  z[0] = n;
   for (int i = 1; i < n; i++) {
-    while (k > 0 && str[k] != str[i]) {
-      k = pi[k - 1];
+    if (i <= r) {
+      z[i] = (r - i + 1) < z[i - l] ? (r - i + 1) : z[i - l];
     }
-    if (str[k] == str[i]) {
-      k++;
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+      z[i]++;
     }
-    pi[i] = k;
+    if (i + z[i] - 1 > r) {
+      l = i;
+      r = i + z[i] - 1;
+    }
   }
 }
 
-int SearchPrefix(const char* s1, const char* s2, int isPrefix) {
+// Функция для поиска наибольшего префикса и суффикса
+int SearchPrefixSuffix(const char* s1, const char* s2, int isPrefix) {
   int l1 = strlen(s1);
   int l2 = strlen(s2);
-  int pi[MAXLENGTH * 2];
+  int z[MAXLENGTH * 2 + 1];
+
   char combined[MAXLENGTH * 2 + 1];
 
   if (isPrefix) {
-    snprintf(combined, sizeof(combined), "%s#%s", s1, s2);
+    snprintf(combined, sizeof(combined), "%s#%s", s2, s1);
+    ZFunction(combined, z, l1 + l2 + 1);
+
+    return z[l2 + 1];
   }
   else {
-    snprintf(combined, sizeof(combined), "%s#%s", s2, s1);
+    snprintf(combined, sizeof(combined), "%s#%s", s1, s2);
+    ZFunction(combined, z, l1 + l2 + 1);
+
+    int maxZ = 0;
+
+    for (int i = l1 + 1; i < l1 + l2 + 1; i++) {
+      if (z[i] == l1 + l2 - i + 1) {
+        maxZ = z[i];
+      }
+    }
+    return maxZ;
   }
-
-  ComputePrefix(combined, pi, l1 + l2 + 1);
-
-  return pi[l1 + l2];
 }
 
 int main() {
   char s1[MAXLENGTH], s2[MAXLENGTH];
-  scanf("%10000s", s1);
-  scanf("%10000s", s2);
 
-  printf("%d %d\n", SearchPrefix(s1, s2, 1), SearchPrefix(s1, s2, 0));
+  scanf("%s", s1);
+  scanf("%s", s2);
+
+  printf("%d %d\n", SearchPrefixSuffix(s1, s2, 1), SearchPrefixSuffix(s1, s2, 0));
 
   return 0;
 }
