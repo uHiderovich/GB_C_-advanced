38011ca5d7b40cd70a0306962d03d3202cff2a54
Артём Гапеевцев <agapeevtsev@yandex.ru>
2024-08-18 16:59:20 +0800

    ДЗ 4 Задание 3

--- HW4/task3.c
+++ HW4/task3.c
@@ -14,7 +14,51 @@ python_has_list_comprehensions
 Данные на выход: 6
 */
 
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLENGTH 10001
+
+void ComputePrefix(const char* str, int* pi, int n) {
+  int k = 0;
+  pi[0] = 0;
+
+  for (int i = 1; i < n; i++) {
+    while (k > 0 && str[k] != str[i]) {
+      k = pi[k - 1];
+    }
+    if (str[k] == str[i]) {
+      k++;
+    }
+    pi[i] = k;
+  }
+}
+
+int SearchPrefix(const char* s1, const char* s2, int isPrefix) {
+  int l1 = strlen(s1);
+  int l2 = strlen(s2);
+  int pi[MAXLENGTH * 2];
+  char combined[MAXLENGTH * 2 + 1];
+
+  if (isPrefix) {
+    snprintf(combined, sizeof(combined), "%s#%s", s1, s2);
+  }
+  else {
+    snprintf(combined, sizeof(combined), "%s#%s", s2, s1);
+  }
+
+  ComputePrefix(combined, pi, l1 + l2 + 1);
+
+  return pi[l1 + l2];
+}
+
 int main() {
-  /* code */
+  char s1[MAXLENGTH], s2[MAXLENGTH];
+  scanf("%10000s", s1);
+  scanf("%10000s", s2);
+
+  printf("%d %d\n", SearchPrefix(s1, s2, 1), SearchPrefix(s1, s2, 0));
+
   return 0;
 }
+
