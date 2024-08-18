35486bf44fa398932f412bce98b00eb065eda3b3
Артём Гапеевцев <agapeevtsev@yandex.ru>
2024-08-18 16:39:08 +0800

    ДЗ 4 Задание 2

--- HW4/task2.c
+++ HW4/task2.c
@@ -11,7 +11,73 @@
 Данные на выход: 8 2 5 & ^ 6 1 & |
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <locale.h>
+
+#define MAX 100
+
+int precedence(char operator) {
+  switch (operator) {
+  case '|': return 1;
+  case '^': return 2;
+  case '&': return 3;
+  default: return 0;
+  }
+}
+
+void infixToPostfix(const char* expression, char* output) {
+  char stack[MAX];
+  int top = -1;
+  int i, k = 0;
+
+  for (i = 0; expression[i]; i++) {
+    if (isspace(expression[i])) continue;
+
+    if (isdigit(expression[i])) {
+      output[k++] = expression[i++];
+      output[k++] = ' ';
+      i--;
+    }
+    else if (expression[i] == '(') {
+      stack[++top] = expression[i];
+    }
+    else if (expression[i] == ')') {
+      while (top >= 0 && stack[top] != '(') {
+        output[k++] = stack[top--];
+        output[k++] = ' ';
+      }
+      top--;
+    }
+    else {
+      while (top >= 0 && precedence(stack[top]) >= precedence(expression[i])) {
+        output[k++] = stack[top--];
+        output[k++] = ' ';
+      }
+      stack[++top] = expression[i];
+    }
+  }
+
+  while (top >= 0) {
+    output[k++] = stack[top--];
+    output[k++] = ' ';
+  }
+}
+
 int main() {
-  /* code */
+  setlocale(LC_ALL, "Rus");
+
+  char expression[MAX];
+  char output[MAX];
+
+  fgets(expression, MAX, stdin);
+
+  infixToPostfix(&expression, &output);
+
+  printf("Результат: %s\n", output);
+
   return 0;
 }
+
