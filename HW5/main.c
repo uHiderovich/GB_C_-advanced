f07313cc4ba4115e97651c2abbb96ff8384e0ab1
Артём Гапеевцев <a.gapeevtsev@samolet.ru>
2024-08-24 09:25:39 +0800

    ДЗ 5 Орректировка поедания еды

--- HW5/main.c
+++ HW5/main.c
@@ -3,34 +3,108 @@
 #include <conio.h>
 #include <unistd.h>
 #include <stdint.h>
-#include "snake.h"
 
-int isTwoPlayers(enum game_modes_t game_mode) {
-  return game_mode == TWO_PLAYERS || game_mode == AI;
+#define SET_RANDOM(min, max) ((min) + rand() % ((max) - (min) + 1))
+
+#define STRAT_X 10
+#define START_Y 10
+#define MAX_X 30
+#define MAX_Y 30
+#define START_SNAKE_SIZE 2
+#define START_TIME 1000000
+#define TIME_STEP 100000
+#define MAX_SIZE 10
+
+#define RESET_COLOR "\033[0m"
+
+enum colors_t {
+  RED = 1,
+  GREEN = 2,
+  YELLOW = 3,
+  WHITE = 4,
+};
+
+enum game_modes_t {
+  SINGLE_PLAYER = 1,
+  TWO_PLAYERS = 2,
+  AI = 3,
+};
+
+enum controls_t {
+  KEY_UP = 119,    // w
+  KEY_RIGHT = 100, // d
+  KEY_DOWN = 115,  // s
+  KEY_LEFT = 97,   // a
+
+  KEY_UP_2 = 72,    // СЃС‚СЂРµР»РєР° РІРІРµСЂС…
+  KEY_RIGHT_2 = 77, // СЃС‚СЂРµР»РєР° РІРїСЂР°РІРѕ
+  KEY_DOWN_2 = 80,  // СЃС‚СЂРµР»РєР° РІРЅРёР·
+  KEY_LEFT_2 = 75,  // СЃС‚СЂРµР»РєР° РІР»РµРІРѕ
+
+  KEY_STOP_GAMGE = 27,
+  KEY_PAUSE_GAMGE = 112,
+};
+
+enum direction_t {
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN
+};
+
+typedef struct tail_t {
+  uint8_t x;
+  uint8_t y;
+} tail_t;
+
+typedef struct snake_t {
+  uint8_t x;
+  uint8_t y;
+  uint8_t tsize;
+  enum colors_t color;
+  enum direction_t direction;
+  enum controls_t controls;
+  struct tail_t* tail;
+} snake_t;
+
+typedef struct food_t {
+  uint8_t x;
+  uint8_t y;
+  enum colors_t color;
+} food_t;
+
+typedef struct game_config_t {
+  enum game_modes_t game_mode;
+  enum colors_t snake_color;
+  enum colors_t food_color;
+} game_config_t;
+
+int isTwoPlayers(game_config_t game_config) {
+  return game_config.game_mode == TWO_PLAYERS || game_config.game_mode == AI;
 }
 
-int isCoorectFoodPosition(snake_t snake, food_t food) {
-  for (size_t i = 0; i < snake.tsize; i++) {
-    if (snake.tail[i].x == food.x && snake.tail[i].y == food.y) {
+int isCoorectFoodPosition(snake_t* snake, food_t* food) {
+  for (size_t i = 0; i < snake->tsize; i++) {
+    if (snake->tail[i].x == food->x && snake->tail[i].y == food->y) {
       return 0;
     }
   }
 
-  return snake.x != food.x && snake.y != food.y;
+  return snake->x != food->x && snake->y != food->y;
 }
 
-void updateFoodPosition(snake_t snake, food_t* food, uint8_t max_x, uint8_t max_y) {
+void updateFoodPosition(snake_t* snake, food_t* food, uint8_t max_x, uint8_t max_y) {
   while (1) {
     food->x = SET_RANDOM(0, max_x);
     food->y = SET_RANDOM(0, max_y);
 
-    if (isCoorectFoodPosition(snake, *food)) {
+    if (isCoorectFoodPosition(snake, food)) {
       break;
     }
   }
 }
 
-char getColor(enum colors_t color) {
+const char* getColor(enum colors_t color) {
   switch (color) {
   case RED:
     return "\033[1;31m";
@@ -47,23 +121,23 @@ char getColor(enum colors_t color) {
   }
 }
 
-food_t initFood(snake_t snake, uint8_t max_x, uint8_t max_y, enum colors_t color) {
+food_t initFood(snake_t snake, uint8_t max_x, uint8_t max_y, game_config_t game_config) {
   food_t food;
-  food.color = color;
+  food.color = game_config.food_color;
 
-  updateFoodPosition(snake, &food, max_x, max_y);
+  updateFoodPosition(&snake, &food, max_x, max_y);
 
   return food;
 }
 
-snake_t initSnake(uint8_t x, uint8_t y, enum colors_t color) {
+snake_t initSnake(uint8_t x, uint8_t y, game_config_t game_config) {
   snake_t snake;
   snake.x = x;
   snake.y = y;
   snake.tsize = START_SNAKE_SIZE;
   snake.tail = (tail_t*)malloc(sizeof(tail_t) * 100);
   snake.direction = LEFT;
-  snake.color = color;
+  snake.color = game_config.snake_color;
 
   for (uint8_t i = 0; i < snake.tsize; ++i) {
     snake.tail[i].x = x + i + 1;
@@ -80,29 +154,39 @@ void addTail(snake_t* snake) {
 void addSnakeOnField(snake_t snake, char headSymbol, char field[][MAX_Y]) {
   field[snake.x][snake.y] = headSymbol;
   for (size_t i = 0; i < snake.tsize; ++i) {
-    // field[snake.tail[i].x][snake.tail[i].y] = getColor(snake.color) + '*' + "\033[0m";
-    snprintf(field[snake.tail[i].x][snake.tail[i].y], 3, "%s%s%s", getColor(snake.color), '*', "\033[0m");
+    field[snake.tail[i].x][snake.tail[i].y] = '*';
   }
 }
 
 // @**
-void printGameField(snake_t snake, snake_t snake2, food_t food, enum game_modes_t game_mode) {
-  char matrix[MAX_X][MAX_Y];
+void printGameField(snake_t snake, snake_t snake2, food_t food, game_config_t game_config) {
+  char game_field[MAX_X][MAX_Y];
 
   for (int i = 0; i < MAX_X; ++i) {
     for (int j = 0; j < MAX_Y; ++j) {
-      matrix[i][j] = ' ';
+      game_field[i][j] = ' ';
     }
   }
 
-  addSnakeOnField(snake, '@', matrix);
-  if (isTwoPlayers(game_mode)) addSnakeOnField(snake2, '&', matrix);
+  addSnakeOnField(snake, '@', game_field);
+  if (isTwoPlayers(game_config)) addSnakeOnField(snake2, '&', game_field);
 
-  matrix[food.x][food.y] = getColor(food.color) + '*' + "\033[0m";
+  game_field[food.x][food.y] = '$';
 
   for (int j = 0; j < MAX_Y; ++j) {
     for (int i = 0; i < MAX_X; ++i) {
-      printf("%c%s", matrix[i][j]);
+      printf("%c", game_field[i][j]);
+
+      // РћРєСЂР°СЃ Р·РјРµР№РєРё Рё РµРґС‹
+      // if (game_field[i][j] == '*' || game_field[i][j] == '@') {
+      //   printf("%s%c%s", getColor(game_config.snake_color), game_field[i][j], RESET_COLOR);
+      // }
+      // else if (game_field[i][j] == '$') {
+      //   printf("%s%c%s", getColor(game_config.food_color), game_field[i][j], RESET_COLOR);
+      // }
+      // else {
+      //   printf("%c", game_field[i][j]);
+      // }
     }
     printf("\n");
   }
@@ -187,11 +271,15 @@ void checkPause(_Bool* isPaused, uint8_t keyDirection) {
 
 int checkIntersectFood(snake_t* snake, food_t* food) {
   if (snake->x == food->x && snake->y == food->y) {
-    addTail(&snake);
-    updateFoodPosition(*snake, &food, MAX_X, MAX_Y);
+    addTail(snake);
+    updateFoodPosition(snake, food, MAX_X, MAX_Y);
     return 1;
   }
   return 0;
+}
+
+int isNewLevel(snake_t* snake, snake_t* snake2, food_t* food, game_config_t game_config) {
+  return checkIntersectFood(snake, food) || (isTwoPlayers(game_config) && checkIntersectFood(snake2, food));
 }
 
 int isCrush(snake_t snake) {
@@ -241,20 +329,18 @@ void play(game_config_t game_config) {
   uint8_t level = 1;
   _Bool isPaused = 0;
 
-  snake_t snake = initSnake(MAX_X / 3, MAX_Y / 3, game_config.snake_color);
-  snake_t snake2;
+  snake_t snake = initSnake(MAX_X / 3, MAX_Y / 3, game_config);
+  snake_t snake2 = initSnake(MAX_X / 2, MAX_Y / 2, game_config);
 
-  if (isTwoPlayers(game_config.game_mode)) snake2 = initSnake(MAX_X / 2, MAX_Y / 2, game_config.snake_color);
+  food_t food = initFood(snake, MAX_X, MAX_Y, game_config);
 
-  food_t food = initFood(snake, MAX_X, MAX_Y, game_config.food_color);
-
-  printGameField(snake, snake2, food, game_config.game_mode);
+  printGameField(snake, snake2, food, game_config);
 
   while (keyDirection != KEY_STOP_GAMGE) {
     if (kbhit()) {
       keyDirection = getch();
       setDirectionPlayer1(&snake, keyDirection);
-      if (isTwoPlayers(game_config.game_mode)) setDirectionPlayer2(&snake2, keyDirection);
+      if (isTwoPlayers(game_config)) setDirectionPlayer2(&snake2, keyDirection);
       checkPause(&isPaused, keyDirection);
     }
 
@@ -265,7 +351,7 @@ void play(game_config_t game_config) {
     }
     else {
       move(&snake);
-      if (isTwoPlayers(game_config.game_mode)) move(&snake2);
+      if (isTwoPlayers(game_config)) move(&snake2);
     }
 
     if (isGameOver(snake, snake2)) {
@@ -275,12 +361,11 @@ void play(game_config_t game_config) {
     usleep(START_TIME - (TIME_STEP * level));
     system("cls");
 
-    if (
-      checkIntersectFood(&snake, &food) || (isTwoPlayers(game_config.game_mode) && checkIntersectFood(&snake2, &food))) {
+    if (isNewLevel(&snake, &snake2, &food, game_config)) {
       level++;
     }
 
-    printGameField(snake, snake2, food, game_config.game_mode);
+    printGameField(snake, snake2, food, game_config);
   }
 
   printf("Game Over!\n");
@@ -328,10 +413,10 @@ void startMenu(game_config_t* game_config) {
 
   game_config->game_mode = selectGameMode();
 
-  printf("Choose the color of the snakes\n");
+  printf("Choose the color of the snakes and press Enter\n");
   game_config->snake_color = selectColor();
 
-  printf("Choose the color of the food\n");
+  printf("Choose the color of the food and press Enter\n");
   game_config->food_color = selectColor();
 }
 
