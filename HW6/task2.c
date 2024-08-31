a666dab96f63f24aaadf70e0d15a51b15f967d17
Артём Гапеевцев <agapeevtsev@yandex.ru>
2024-08-31 12:28:10 +0800

    ДЗ 6 Задание 2

--- HW6/task2.c
+++ HW6/task2.c
@@ -4,7 +4,44 @@
 Данные на выходе: 78
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node {
+  int value;
+  struct Node* left;
+  struct Node* right;
+} Node;
+
+Node* createNode(int data) {
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  newNode->value = data;
+  newNode->left = NULL;
+  newNode->right = NULL;
+  return newNode;
+}
+
+int sumTree(Node* root) {
+  if (root == NULL) {
+    return 0;
+  }
+  return root->value + sumTree(root->left) + sumTree(root->right);
+}
+
 int main() {
-  /* code */
+  Node* root = createNode(10);
+  root->left = createNode(5);
+  root->right = createNode(15);
+  root->left->left = createNode(3);
+  root->left->right = createNode(7);
+  root->right->left = createNode(13);
+  root->right->right = createNode(18);
+  root->left->left->left = createNode(1);
+  root->left->right->left = createNode(6);
+
+  int sum = sumTree(root);
+  printf("Sum: %d\n", sum);
+
   return 0;
 }
