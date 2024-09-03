b39d391454a03e44fbdfb01c9d0d71fd0a870626
Артём Гапеевцев <a.gapeevtsev@samolet.ru>
2024-09-03 20:13:29 +0800

    Иттговая работа: Поломка дрона, много дронов

--- result/main.c
+++ result/main.c
@@ -14,9 +14,10 @@
 #define MAX_X 30
 #define MAX_Y 30
 #define START_TRAILER_SIZE 1
-#define SLEEP 700000
+#define SLEEP 500000
 #define MAX_SIZE 10
-#define PURPOSE_COUNT 5
+#define PURPOSES_COUNT 5
+#define DRONES_COUNT 1
 
 enum Controls {
   KEY_UP = 119,    // w
@@ -47,54 +48,76 @@ typedef struct Drone {
   enum Direction direction;
   enum Controls controls;
   struct Trailer *trailer;
+  _Bool enable;
 } Drone;
 
 typedef struct Purpose {
   uint8_t x;
   uint8_t y;
+  _Bool enable;
 } Purpose;
 
-Purpose *initPurposes() {
-  Purpose *purposes = (Purpose *)malloc(sizeof(Purpose) * PURPOSE_COUNT);
+void updatePurposePosition(Purpose *purpose) {
+  purpose->x = SET_RANDOM(0, MAX_X);
+  purpose->y = SET_RANDOM(0, MAX_Y);
+}
 
-  for (size_t i = 0; i < PURPOSE_COUNT; i++) {
+Purpose *initPurposes() {
+  Purpose *purposes = (Purpose *)malloc(sizeof(Purpose) * PURPOSES_COUNT);
+
+  for (size_t i = 0; i < PURPOSES_COUNT; i++) {
     Purpose purpose;
-    purpose.x = SET_RANDOM(0, MAX_X);
-    purpose.y = SET_RANDOM(0, MAX_Y);
+    updatePurposePosition(&purpose);
+    purpose.enable = 1;
     purposes[i] = purpose;
   }
 
   return purposes;
 }
 
-Drone initDrone(uint8_t x, uint8_t y) {
-  Drone drone;
-  drone.x = x;
-  drone.y = y;
-  drone.tsize = START_TRAILER_SIZE;
-  drone.trailer = (Trailer *)malloc(sizeof(Trailer) * MAX_SIZE);
-  drone.direction = LEFT;
+Drone *initDrones() {
+  Drone *drones = (Drone *)malloc(sizeof(Drone) * DRONES_COUNT);
 
-  for (uint8_t i = 0; i < drone.tsize; ++i) {
-    drone.trailer[i].x = x + i + 1;
-    drone.trailer[i].y = y;
+  for (size_t i = 0; i < DRONES_COUNT; i++) {
+    Drone drone;
+    drone.x = SET_RANDOM(0, MAX_X);
+    drone.y = SET_RANDOM(0, MAX_Y);
+    drone.tsize = START_TRAILER_SIZE;
+    drone.trailer = (Trailer *)malloc(sizeof(Trailer) * MAX_SIZE);
+    drone.direction = LEFT;
+    drone.enable = 1;
+
+    for (uint8_t i = 0; i < drone.tsize; ++i) {
+      drone.trailer[i].x = drone.x + i + 1;
+      drone.trailer[i].y = drone.y;
+    }
+
+    drones[i] = drone;
   }
 
-  return drone;
+  return drones;
 }
 
-void addTrailer(Drone *drone) {
-  drone->tsize++;
-}
+void addDronesOnArea(Drone *drones, char area[][MAX_Y]) {
+  for (size_t i = 0; i < DRONES_COUNT; i++) {
+    area[drones[i].x][drones[i].y] = '@';
 
-void addDroneOnArea(Drone drone, char area[][MAX_Y]) {
-  area[drone.x][drone.y] = '@';
-  for (size_t i = 0; i < drone.tsize; ++i) {
-    area[drone.trailer[i].x][drone.trailer[i].y] = '*';
+    for (size_t j = 0; j < drones[i].tsize; j++) {
+      area[drones[i].trailer[j].x][drones[i].trailer[j].y] = '*';
+    }
+
   }
 }
 
-void printWorkArea(Drone drone, Purpose *purposes) {
+void addPurposeOnArea(Purpose *purposes, char area[][MAX_Y]) {
+  for (size_t i = 0; i < PURPOSES_COUNT; i++) {
+    if (purposes[i].enable) {
+      area[purposes[i].x][purposes[i].y] = '$';
+    }
+  }
+}
+
+void printWorkArea(Drone *drones, Purpose *purposes) {
   char work_area[MAX_X][MAX_Y];
 
   for (int i = 0; i < MAX_X; ++i) {
@@ -103,11 +126,9 @@ void printWorkArea(Drone drone, Purpose *purposes) {
     }
   }
 
-  for (size_t i = 0; i < PURPOSE_COUNT; i++) {
-    work_area[purposes[i].x][purposes[i].y] = '$';
-  }
+  addPurposeOnArea(purposes, work_area);
 
-  addDroneOnArea(drone, work_area);
+  addDronesOnArea(drones, work_area);
 
   for (int j = 0; j < MAX_Y; ++j) {
     for (int i = 0; i < MAX_X; ++i) {
@@ -166,6 +187,21 @@ void checkPause(_Bool *isPaused, uint8_t keyDirection) {
   }
 }
 
+int isMissionCompleted(Drone drone) {
+  return drone.tsize == MAX_SIZE;
+}
+
+void checkIntersectPurpose(Drone *drones, Purpose *purposes) {
+  for (size_t i = 0; i < DRONES_COUNT; i++) {
+    for (size_t j = 0; j < PURPOSES_COUNT; j++) {
+      if (drones[i].x == purposes[j].x && drones[i].y == purposes[j].y) {
+        drones[i].tsize++;
+        purposes[j].enable = 0;
+      }
+    }
+  }
+}
+
 int isCrush(Drone drone) {
   for (size_t i = 0; i < drone.tsize; i++) {
     if (drone.trailer[i].x == drone.x && drone.trailer[i].y == drone.y) {
@@ -176,30 +212,45 @@ int isCrush(Drone drone) {
   return 0;
 }
 
-int isMissionCompleted(Drone drone) {
-  return drone.tsize == MAX_SIZE || isCrush(drone);
-};
+void checkCrushedDrone(Drone *drones) {
+  for (size_t i = 0; i < DRONES_COUNT; i++) {
+    if (drones[i].enable && isCrush(drones[i])) {
+      drones[i].enable = 0;
+      printf("Drone %d is crushed!\n", (int)i + 1);
+    }
+  }
+}
 
-Drone move(Drone *drone) {
-  for (size_t i = drone->tsize - 1; i > 0; i--) {
-    drone->trailer[i] = drone->trailer[i - 1];
+_Bool isAllDronesCrushed(Drone *drones) {
+  for (size_t i = 0; i < DRONES_COUNT; i++) {
+    if (drones[i].enable) {
+      return 0;
+    }
   }
 
-  drone->trailer[0].x = drone->x;
-  drone->trailer[0].y = drone->y;
+  return 1;
+}
 
-  switch (drone->direction) {
+void move(Drone drone) {
+  for (size_t i = drone.tsize - 1; i > 0; i--) {
+    drone.trailer[i] = drone.trailer[i - 1];
+  }
+
+  drone.trailer[0].x = drone.x;
+  drone.trailer[0].y = drone.y;
+
+  switch (drone.direction) {
   case LEFT:
-    drone->x = GET_MAX(drone->x - 1, 0);
+    drone.x = GET_MAX(drone.x - 1, 0);
     break;
   case RIGHT:
-    drone->x = GET_MIN(drone->x + 1, MAX_X);
+    drone.x = GET_MIN(drone.x + 1, MAX_X);
     break;
   case UP:
-    drone->y = GET_MAX(drone->y - 1, 0);
+    drone.y = GET_MAX(drone.y - 1, 0);
     break;
   case DOWN:
-    drone->y = GET_MIN(drone->y + 1, MAX_Y);
+    drone.y = GET_MIN(drone.y + 1, MAX_Y);
     break;
   default:
     break;
@@ -210,32 +261,42 @@ Drone move(Drone *drone) {
 
 void start() {
   uint8_t keyDirection = 0;
+  _Bool isAllCrused = 0;
 
-  Drone drone = initDrone(MAX_X / 2, MAX_Y / 2);
+  Drone *drones = initDrones();
   Purpose *purposes = initPurposes();
 
-  printWorkArea(drone, purposes);
+  printWorkArea(drones, purposes);
 
   while (keyDirection != KEY_STOP_GAMGE) {
-    if (kbhit()) {
-      keyDirection = getch();
-      setDirection(&drone, keyDirection);
+    // if (kbhit()) {
+    //   keyDirection = getch();
+    //   setDirection(&drone, keyDirection);
+    // }
 
+    for (size_t i = 0; i < DRONES_COUNT; i++) {
+      move(drones[i]);
     }
 
-    move(&drone);
+    checkIntersectPurpose(drones, purposes);
 
-    if (isMissionCompleted(drone)) {
+    checkCrushedDrone(drones);
+
+    if (isAllCrused = isAllDronesCrushed(drones)) {
       break;
     }
 
     usleep(SLEEP);
     system("cls");
 
-    printWorkArea(drone, purposes);
+    printWorkArea(drones, purposes);
   }
 
-  printf("Mission completed!\n");
+  if (isAllCrused) {
+    printf("All drones are crushed! =(");
+  }
+
+  // printf("Mission completed!\n");
 }
 
 
